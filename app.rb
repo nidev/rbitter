5eaa4e3a3fba27556b0616a050918522905cfa8e
Nidev Plontra <nidev.plontra@gmail.com>
2015-01-21 17:45:45 +0900

    indentation size changed to 2 spaces, from one tab

--- app.rb
+++ app.rb
@@ -7,36 +7,36 @@ require "./streaming"
 require "./dlthread"
 
 module Application
-	class ConfigLoader
-		def initialize(cfg_filename='config.json')
-			File.open(cfg_filename, "r") { |file|
-				@cfg = JSON.parse(file.read)
-			}
-		end
+  class ConfigLoader
+    def initialize(cfg_filename='config.json')
+      File.open(cfg_filename, "r") { |file|
+        @cfg = JSON.parse(file.read)
+      }
+    end
 
-		def method_missing(method_name, *args)
-			if method_name.class == String
-				method_name = method_name.to_sym
-			end
-			
-			@cfg.__send__(method_name, *args)
-		end
-	end
+    def method_missing(method_name, *args)
+      if method_name.class == String
+        method_name = method_name.to_sym
+      end
+      
+      @cfg.__send__(method_name, *args)
+    end
+  end
 
-	class Main
-		def initialize
-			@cl = ConfigLoader.new
-			@t = StreamClient.new(@cl['twitter'].dup)
-			@d = Database::DBHandler.new(:host => @cl['mysql2']['host'], :port => @cl['mysql2']['port'], :dbname => @cl['mysql2']['dbname'])
-			@d.link(@cl['mysql2']['username'], @cl['mysql2']['password'])
-			@d.create_if_not_exists(@cl['mysql2']['tablename'])
-			@tablename = @cl['mysql2']['tablename']
+  class Main
+    def initialize
+      @cl = ConfigLoader.new
+      @t = StreamClient.new(@cl['twitter'].dup)
+      @d = Database::DBHandler.new(:host => @cl['mysql2']['host'], :port => @cl['mysql2']['port'], :dbname => @cl['mysql2']['dbname'])
+      @d.link(@cl['mysql2']['username'], @cl['mysql2']['password'])
+      @d.create_if_not_exists(@cl['mysql2']['tablename'])
+      @tablename = @cl['mysql2']['tablename']
 
-			@dt = DLThread.new(@cl['media_downloader']['download_dir'], @cl['media_downloader']['cacert_path'])
-		end
+      @dt = DLThread.new(@cl['media_downloader']['download_dir'], @cl['media_downloader']['cacert_path'])
+    end
 
-		def write_marker(message)
-			@d.insert_into(@tablename, :id => nil,
+    def write_marker(message)
+      @d.insert_into(@tablename, :id => nil,
         :marker => 1,
         :marker_msg => message, 
         :userid => nil,
@@ -46,40 +46,40 @@ module Application
         :date => nil,
         :rt_count => 0,
         :fav_count => 0)
-		end
+    end
 
-		def write_init_marker
-			write_marker "recording started"
-		end
+    def write_init_marker
+      write_marker "recording started"
+    end
 
-		def write_halt_marker
-			write_marker "recording halted"
-		end
+    def write_halt_marker
+      write_marker "recording halted"
+    end
 
-		def run
-			write_init_marker
-			@t.run { |a|
-				@d.insert_into(@tablename, :id => nil,
-					:marker => 0,
-					:marker_msg => "normal", 
-					:userid => a['userid'],
-					:username => a['screen_name'],
-					:tweetid => a['tweetid'],
-					:tweet => a['tweet'],
-					:date => a['date'],
-					:rt_count => a['rt_count'],
-					:fav_count => a['fav_count'])
-				
-				# Image download
-				puts a['tweet']
-				@dt.execute_urls(a['urls'])
-			}
-			write_halt_marker
-		end
-	end
+    def run
+      write_init_marker
+      @t.run { |a|
+        @d.insert_into(@tablename, :id => nil,
+          :marker => 0,
+          :marker_msg => "normal", 
+          :userid => a['userid'],
+          :username => a['screen_name'],
+          :tweetid => a['tweetid'],
+          :tweet => a['tweet'],
+          :date => a['date'],
+          :rt_count => a['rt_count'],
+          :fav_count => a['fav_count'])
+        
+        # Image download
+        puts a['tweet']
+        @dt.execute_urls(a['urls'])
+      }
+      write_halt_marker
+    end
+  end
 end
 
 if __FILE__ == $0
-	main = Application::Main.new
-	main.run
+  main = Application::Main.new
+  main.run
 end
--- streaming.rb
+++ streaming.rb
@@ -3,73 +3,73 @@
 require 'twitter'
 
 class StreamClient
-	def initialize(tokens)
-		@t = Twitter::Streaming::Client.new do |object|
-			object.consumer_key        = tokens['consumer_key']
-			object.consumer_secret     = tokens['consumer_secret']
-			object.access_token        = tokens['access_token']
-			object.access_token_secret = tokens['access_token_secret']
-		end
-	end
+  def initialize(tokens)
+    @t = Twitter::Streaming::Client.new do |object|
+      object.consumer_key        = tokens['consumer_key']
+      object.consumer_secret     = tokens['consumer_secret']
+      object.access_token        = tokens['access_token']
+      object.access_token_secret = tokens['access_token_secret']
+    end
+  end
 
-	def run(&operation_block)
-		begin
-			internal(&operation_block)
-		rescue EOFError => e
-			puts "Network unavailable. restart in 3 seconds..."
-			sleep 3
-			retry
-		rescue Exception => e
-			puts "Exception (#{e.inspect})"
-			#Kernel.exit(-1)
-		end
-	end
+  def run(&operation_block)
+    begin
+      internal(&operation_block)
+    rescue EOFError => e
+      puts "Network unavailable. restart in 3 seconds..."
+      sleep 3
+      retry
+    rescue Exception => e
+      puts "Exception (#{e.inspect})"
+      #Kernel.exit(-1)
+    end
+  end
 
-	private
-	def internal(&operation_block)
-		@t.user do |tweet|
-			if tweet.is_a?(Twitter::Tweet)
-				text = tweet.full_text.gsub(/(\r\n|\n)/, '')
+  private
+  def internal(&operation_block)
+    @t.user do |tweet|
+      if tweet.is_a?(Twitter::Tweet)
+        text = tweet.full_text.gsub(/(\r\n|\n)/, '')
 
-				# unpack uris and media links
-				urls = Array.new
+        # unpack uris and media links
+        urls = Array.new
 
-				if tweet.entities?
-					if tweet.media?
-						tweet.media.each { |uri|
-							urls.push("#{uri.media_uri_https}")
-							text.gsub!("#{uri.uri}", "#{uri.media_uri_https}")
-						}
-					end
+        if tweet.entities?
+          if tweet.media?
+            tweet.media.each { |uri|
+              urls.push("#{uri.media_uri_https}")
+              text.gsub!("#{uri.uri}", "#{uri.media_uri_https}")
+            }
+          end
 
-					if tweet.uris?
-						tweet.uris.each { |uri|
-							#urls.push("#{uri.expanded_url}")
-							text.gsub!("#{uri.url}", "#{uri.expanded_url}")
-						}
-					end
-				end
+          if tweet.uris?
+            tweet.uris.each { |uri|
+              #urls.push("#{uri.expanded_url}")
+              text.gsub!("#{uri.url}", "#{uri.expanded_url}")
+            }
+          end
+        end
 
-				res = {
-					"tweetid" => tweet.id,
-					"userid" => tweet.user.id,
-					"tweet" => text,
-					"rt_count" => tweet.retweet_count,
-					"fav_count" => tweet.favorite_count,
-					"screen_name" => tweet.user.screen_name,
-					"date" => tweet.created_at,
-					"urls" => urls
-				}
-				begin
-					operation_block.call(res)
-				rescue Mysql2::Error => e
-					puts "======================================"
-					puts "Mysql2::Error exception caught. (#{e.to_s})"
-					puts "It seemed to be an encoding exception. (given encoding : #{res['tweet'].encoding})"
-					# XXX: RubyInstaller 2.1.1 encoding issue.
-					#res['tweet'].encode!("UTF-8", "UTF-8", { :invalid => :replace, :undef => :replace, :replace => "?" })
-				end
-			end			
-		end
-	end
+        res = {
+          "tweetid" => tweet.id,
+          "userid" => tweet.user.id,
+          "tweet" => text,
+          "rt_count" => tweet.retweet_count,
+          "fav_count" => tweet.favorite_count,
+          "screen_name" => tweet.user.screen_name,
+          "date" => tweet.created_at,
+          "urls" => urls
+        }
+        begin
+          operation_block.call(res)
+        rescue Mysql2::Error => e
+          puts "======================================"
+          puts "Mysql2::Error exception caught. (#{e.to_s})"
+          puts "It seemed to be an encoding exception. (given encoding : #{res['tweet'].encoding})"
+          # XXX: RubyInstaller 2.1.1 encoding issue.
+          #res['tweet'].encode!("UTF-8", "UTF-8", { :invalid => :replace, :undef => :replace, :replace => "?" })
+        end
+      end      
+    end
+  end
 end
