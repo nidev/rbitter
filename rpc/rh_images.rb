88763aad0a54d54e30f0190db07c8802d21f1736
Nidev Plontra <nidev.plontra@gmail.com>
2015-01-31 19:31:19 +0900

    Add new RPCHandle: image hosting

---
+++ rpc/rh_images.rb
@@ -0,0 +1,31 @@
+# encoding: utf-8
+#
+
+require "base64"
+require_relative "base"
+
+module RPCHandles
+  class ImageHost < Auth
+    attr_accessor :desc
+    def initialize
+      @desc = RH_INFO.new("imghost", "0.1", "nidev", "Host images downloaded from Tweets.")
+      puts @desc.digest
+
+      conf = Application::ConfigLoader.new
+      @imgroot = File.expand_path(conf['media_downloader']['download_dir'])
+    end
+
+    def image link_or_filename
+      exact_fname = File.basename(link_or_filename)
+      imgpath = File.join(@imgroot, exact_fname)
+      if exact_fname.empty? or not File.exist?(imgpath)
+        ""
+      else
+        b64_data = open(imgpath, 'rb') { |io|
+          Base64.encode64(io.read)
+        }
+        b64_data
+      end
+    end
+  end
+end
