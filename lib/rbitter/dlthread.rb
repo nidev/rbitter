d82a986755bc4050d4a29b1c24f58f6d83b2ec2c
Nidev Plontra <nidev.plontra@gmail.com>
2015-08-23 17:00:59 +0900

    DLThread pool managing behavior updated.
    All thread will be join()ed before leaving job_cleanup scope.

--- lib/rbitter/dlthread.rb
+++ lib/rbitter/dlthread.rb
@@ -25,12 +25,17 @@ module Rbitter
     end
 
     def <<(url_array)
+      if @pool.length >= MAX_THREADS
+        job_cleanup
+      else
+        puts "[dlthread] Finishing thread [remains: #{@pool.length}]"
+      end
+
       download_task = Thread.new do
         url_array.each { |url|
           uri = URI.parse(@large_image ? url + ":large" : url) 
-          ssl = uri.scheme.downcase == 'https'
 
-          Net::HTTP.start(uri.host, uri.port, :use_ssl => ssl) { |h|
+          Net::HTTP.start(uri.host, uri.port, :use_ssl => uri.scheme.downcase == 'https') { |h|
             req = Net::HTTP::Get.new uri.request_uri
             h.request(req) { |res|
               case res
@@ -47,21 +52,16 @@ module Rbitter
         }
       end
 
-      if @pool.length >= MAX_THREADS
-	job_cleanup
-      end
-
       @pool.push download_task
     end
 
     def job_cleanup
       until @pool.empty?
         dlthrd = @pool.shift
-
-        if dlthrd.alive?
-          puts "[dlthread] Thread forceful cleaning up [remains: #{@pool.length}]"
-          dlthrd.terminate
-          dlthrd.join
+        
+        puts "[dlthread] Finishing thread [remains: #{@pool.length}]"
+        if dlthrd.join(5).nil?
+          puts "[dlthread] #{dlthrd.to_s} is still running. (timeout: 5sec)"
         end
       end
     end
