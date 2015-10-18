87de3cf0d7604add08edf93cc1d8d36c3b44d70a
Nidev Plontra <nidev.plontra@gmail.com>
2015-10-18 18:26:07 +0900

    Update overriding code of twitter/connection.rb
    http gem 0.9.8 does not provide port number when uri_object.port is called.
    
    Addressable::URI may be the source of this problem, however calling uri_object.default_port seems reasonable when an authority is given properly, in other words, given port and default port is same.
    
    I monkeypatch this problem until proper fix is applied.

--- lib/rbitter/override/gems/twitter/connection.rb
+++ lib/rbitter/override/gems/twitter/connection.rb
@@ -17,7 +17,9 @@ module Twitter
 
       def stream(request, response)
         client_context = OpenSSL::SSL::SSLContext.new
-        client = @tcp_socket_class.new(Resolv.getaddress(request.uri.host), request.uri.port)
+        client = @tcp_socket_class.new(
+          Resolv.getaddress(request.uri.host),
+          request.uri.port.nil? ? request.uri.default_port : request.uri.port)
         ssl_client = @ssl_socket_class.new(client, client_context)
         ssl_client.connect
         request.stream(ssl_client)
